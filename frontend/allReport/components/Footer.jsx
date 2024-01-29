ba606028db8640fb3c3a82f0b03ff1c367332e65
tong41 <tongtong.workmotion@gmail.com>
2024-01-30 00:22:00 +0700

    all report 2

--- frontend/allReport/components/Footer.jsx
+++ frontend/allReport/components/Footer.jsx
@@ -10,23 +10,12 @@ import SendIcon from '@mui/icons-material/Send';
 import WatchLaterIcon from '@mui/icons-material/WatchLater';
 import SettingsIcon from '@mui/icons-material/Settings';
 
-function refreshMessages() {
-  const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
 
-  return Array.from(new Array(50)).map(
-    () => messageExamples[getRandomInt(messageExamples.length)],
-  );
-}
 
 export default function FixedBottomNavigation() {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState(3);
   const ref = React.useRef(null);
-  const [messages, setMessages] = React.useState(() => refreshMessages());
-
-  React.useEffect(() => {
-    ref.current.ownerDocument.body.scrollTop = 0;
-    setMessages(refreshMessages());
-  }, [value, setMessages]);
+ 
 
   return (
     <Box sx={{ pb: 7 }} ref={ref}>
